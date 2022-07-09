f5391a283ef647a4c7be4599c44be79f535c2891
Chaitanya Bhilange <107292155+ChaitanyaBhilange@users.noreply.github.com>
2022-07-08 22:09:52 -0700

    Add files via upload

---
+++ index.py
@@ -0,0 +1,19 @@
+#index
+s1='YashamAcademy'
+print(s1[0:3])
+print(s1[-3])
+print(s1[0:13])
+print(s1[-14:-1])
+
+
+#slicing ,Step size
+print(s1[0:2])
+print(s1[0::4])
+print(s1[0::2])
+print(s1[0::1])
+print(s1[ ::-2])
+
+#Reversing string
+print(s1[-1::-2])
+print(s1[-1::-3])
+
